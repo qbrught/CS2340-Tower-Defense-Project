21cd0a944175bee65b7727bcf3e8a2b5525a3e74
Liusef <liujoseph2920@Outlook.com>
2022-02-12 17:34:05 -0500

    Created a module and entrypoint for our JFX application

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -13,3 +13,19 @@ module com.example.btd {
     opens com.example.btd to javafx.fxml;
     exports com.example.btd;
 }
+
+module org.glizzygladiators {
+        requires javafx.controls;
+        requires javafx.fxml;
+        requires javafx.web;
+
+        requires org.controlsfx.controls;
+        requires com.dlsc.formsfx;
+        requires validatorfx;
+        requires org.kordamp.ikonli.javafx;
+        requires org.kordamp.bootstrapfx.core;
+        requires eu.hansolo.tilesfx;
+
+        opens com.example.btd to javafx.fxml;
+        exports com.example.btd;
+        }
---
+++ src/main/java/org/glizzygladiators/TowerDefenseApp.java
@@ -0,0 +1,18 @@
+package org.glizzygladiators;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TowerDefenseApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        // TODO Write the code to open a JavaFX stage
+    }
+
+    public static void main(String[] args) {
+        launch(); // TODO Determine whether this needs any args to be passed
+    }
+}
