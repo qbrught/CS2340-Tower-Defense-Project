af6d8ec57d7f8524d797f502820dde69f76d9689
Liusef <32256890+Liusef@users.noreply.github.com>
2022-03-18 21:53:53 -0400

    Wrote the path

--- src/main/java/org/glizzygladiators/td/entities/GameMap.java
+++ src/main/java/org/glizzygladiators/td/entities/GameMap.java
@@ -1,6 +1,15 @@
 package org.glizzygladiators.td.entities;
 
+import javafx.scene.Node;
+import javafx.scene.shape.LineTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
+import javafx.scene.shape.Shape;
+import javafx.util.Pair;
+import org.glizzygladiators.td.entities.enemies.Enemy;
 import org.glizzygladiators.td.entities.towers.Tower;
+
+import java.util.ArrayList;
 
 public class GameMap {
 
@@ -11,29 +20,53 @@ public class GameMap {
         new SymbolicGameObject(125, 364, 169 - 125, 572 - 364, null),
         new SymbolicGameObject(170, 552, 747 - 170, 602 - 552, null)
     };;
-    public int[][] path = {
-        {0, collisionBound[0].y},
-        {795, collisionBound[0].y},
-        {795, collisionBound[2].y},
-        {collisionBound[2].x, collisionBound[2].height},
-        {750, 750},
-        {1000, 1000}
-    };
-    public int[][] offsets = {
-        {1, 0},
-        {0, 1},
-        {-1, 0},
-        {0, 1},
-        {1, 0}
-    };
-    public final int startX;
-    public final int startY;
+
+    private Path enemyPath;
+
+//    public int[][] path = {
+//        {0, collisionBound[0].y},
+//        {795, collisionBound[0].y},
+//        {795, collisionBound[2].y},
+//        {collisionBound[2].x, collisionBound[2].height},
+//        {750, 750},
+//        {1000, 1000}
+//    };
+//    public int[][] offsets = {
+//        {1, 0},
+//        {0, 1},
+//        {-1, 0},
+//        {0, 1},
+//        {1, 0}
+//    };
+//    public final int startX;
+//    public final int startY;
 
     public GameMap() {
-        startX = path[0][0];
-        startY = path[0][1];
+
     }
 
+    private void generatePath() {
+        var es = Enemy.SIZE / 2; // constant to subtract by to account for the center of a rect
+        enemyPath = new Path();
+        var stuff = enemyPath.getElements();
+
+        var p1 = new Pair<>(Enemy.SIZE / 2 - es, 145 - es);
+        var p2 = new Pair<>(810 - es, 145 - es);
+        var p3 = new Pair<>(810 - es, 346 - es);
+        var p4 = new Pair<>(160 - es, 346 - es);
+        var p5 = new Pair<>(160 - es, 576 - es);
+        var p6 = new Pair<>(747 - es, 576 - es);
+        var ps = new Pair[]{p2, p3, p4, p5, p6};
+
+        stuff.add(new MoveTo(p1.getKey(), p1.getValue()));
+        for (var p : ps) stuff.add(new LineTo((Integer)p.getKey(), (Integer)p.getValue()));
+    }
+
+    /**
+     * Whether a tower has a collision with the path
+     * @param input the tower to check against
+     * @return whether the tower collides with the path
+     */
     public boolean hasCollisionWithPath(Tower input) {
         for (var rect: collisionBound) {
             if (input.hasCollision(rect)) {
@@ -41,5 +74,13 @@ public class GameMap {
             }
         }
         return false;
+    }
+
+    /**
+     * Returns the path that enemies move along
+     * @return the path that enemies move along
+     */
+    public Path getEnemyPath() {
+        return enemyPath;
     }
 }
